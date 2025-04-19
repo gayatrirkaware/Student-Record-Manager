8b5ad04f98ecef31638a091544d48a6e942b8b3a
gayatrirkaware <gayatrikawarer@gmail.com>
2025-04-19 12:51:42 +0530

    Added database file

---
+++ app/database.py
@@ -0,0 +1,11 @@
+import pymongo
+import project_config
+
+def get_db():
+    url = f"mongodb://localhost:{project_config.MONGODB_PORT_NUMBER}/"
+    mongo_client = pymongo.MongoClient(url)
+
+    db = mongo_client[project_config.PROJECT_DB_NAME]
+    user_collection = db[project_config.USER_COLLECTION_NAME]
+    
+    return user_collection
